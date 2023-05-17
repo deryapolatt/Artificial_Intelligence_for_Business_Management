a31bc66e1285cdc8bc592222972589a8e7f9c6ff
deryapolatt <105984553+deryapolatt@users.noreply.github.com>
2023-05-17 14:26:45 +0300

    model verimliliği analizi

--- calisanlar_sınıflandırma_modeli.py
+++ calisanlar_sınıflandırma_modeli.py
@@ -39,4 +39,23 @@ for model_name, model in models.items():
 scores_df = pd.DataFrame(scores.items(), columns=["Model", "Score"])
 #skor: accuracy değerleri
 
-print(scores_df)
+#Model Verimliliği Arttırma
+
+#Random Forest Modeli
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+X = df.drop(['Attrition', 'Department', 'EducationField'], axis=1).values
+y = df["Attrition"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+random_forest = RandomForestClassifier(max_depth=5, min_samples_split=5, n_estimators=100)
+
+random_forest.fit(X_train, y_train)
+
+y_pred = random_forest.predict(X_test)
+
+accuracy = random_forest.score(X_test, y_test)
+print("Model Accuracy:", accuracy)
